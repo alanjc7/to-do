d22e469e1330b753312df9d9e569ef65d1192266
Alan Carrie <alan.carrie@onfido.com>
2017-10-03 14:17:31 +0100

    add defailt value false to complete column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002132657) do
+ActiveRecord::Schema.define(version: 20171003131148) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text     "description"
-    t.boolean  "complete"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "description"
+    t.boolean "complete", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
